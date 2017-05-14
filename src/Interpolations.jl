1350c09578352d9933ed9b7d6ff6779ba4f1f89c
Masashi Takahashi <emperor-np@i.softbank.jp>
2017-05-14 15:12:54 +0900

    Update Interpolations.jl

--- src/Interpolations.jl
+++ src/Interpolations.jl
@@ -1,16 +1,5 @@
 module Interpolations
 
-  #書き方を変えてみる
-  function my_lin_interp2(a,b,c,d)
-    
-      function fuck(x)
-        
-          return ((d-c)/(b-a))*(x-a)+b
-        
-      end
-    
-    return fuck
-    
-  end
+
 
 end
