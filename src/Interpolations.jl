1d9d2e5b5f80e6934106f8b4808dc9b3141e29b7
Masashi Takahashi <emperor-np@i.softbank.jp>
2017-05-16 20:11:20 +0900

    Update Interpolations.jl

--- src/Interpolations.jl
+++ src/Interpolations.jl
@@ -15,4 +15,40 @@ end
 
 export linerintep
 
+
+
+function linerinterp(grid,vals)
+     
+    function funfun(x)
+
+        if x < grid[1]
+        
+            return 0
+        
+        end
+    
+        if x > grid[end]
+        
+            return 0
+        
+        end
+    
+        if grid[1] <= x <= grid[end]
+            
+            i = searchsortedlast(grid,x)
+
+            y = (vals[i+1]-vals[i])/(grid[i+1]-grid[i])*(x-grid[i])+vals[i]
+      
+            return y
+            
+        end
+        
+    end
+    
+    return funfun
+        
+end
+
+export linerinterp
+
 end
