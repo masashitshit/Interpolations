684699b9964b98e29391dac0c29ffa9ee9c96db0
DESKTOP-H86FLV2\Masashi Takahashi <emperor-boy@outlook.jp>
2017-05-20 16:33:28 +0900

    no message

--- src/Interpolations.jl
+++ src/Interpolations.jl
@@ -1,6 +1,22 @@
 
 module Interpolations
 
+function linerintep(grid,vals)
+    
+    function funfunc(x)
+        
+        return (vals[2]-vals[1])/(grid[2]-grid[1])*(x-grid[1])+vals[1]
+        
+    end
+    
+    return funfunc
+    
+end
+
+export linerintep
+
+
+
 function linerinterp(grid,vals)
      
     function funfun(x)
