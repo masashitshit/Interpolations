0fdf5e4d44bbf8fcc139ca99ee6ee2a487df7055
Masashi Takahashi <emperor-np@i.softbank.jp>
2017-05-21 21:47:34 +0900

    Update Interpolations.jl

--- src/Interpolations.jl
+++ src/Interpolations.jl
@@ -15,13 +15,13 @@ function linerinterp(grid,vals)
         
         end
     
-          if  grid[end] <x
+          if  grid[end] <= x
         
             return 0
         
         end
     
-        if grid[1] <= x <= grid[end]
+        if grid[1] <= x < grid[end]
             
             i = searchsortedlast(grid,x)
 
